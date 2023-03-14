269d0383391130ca340cb4d1cde277ebbb9e3f14
李洋 <1499271883@qq.com>
2023-03-14 18:39:21 +0800

    add JobRunner

---
+++ jobRunner/src/main/java/com/github/shiayanga/jobrunner/JobRunnerApplication.java
@@ -0,0 +1,16 @@
+package com.github.shiayanga.jobrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LiYang
+ */
+@SpringBootApplication
+public class JobRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobRunnerApplication.class, args);
+	}
+
+}
---
+++ jobRunner/src/test/java/com/github/shiayanga/jobrunner/JobRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.shiayanga.jobrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
