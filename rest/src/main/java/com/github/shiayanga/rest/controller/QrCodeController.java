5765bc9950b82e79adffa523c6512df6f216a0a1
和信金谷-李洋 <1499271883@qq.com>
2023-07-19 11:44:06 +0800

    生成二维码+文字+logo

--- rest/src/main/java/com/github/shiayanga/rest/controller/QrCodeController.java
+++ rest/src/main/java/com/github/shiayanga/rest/controller/QrCodeController.java
@@ -1,14 +1,12 @@
 package com.github.shiayanga.rest.controller;
 
 import com.github.shiayanga.qrcode.QrCodeHelper;
-import com.google.zxing.WriterException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * @author LiYang
@@ -17,7 +15,7 @@ import java.io.IOException;
 @RequestMapping("/qrcode")
 public class QrCodeController {
     @GetMapping("/generate")
-    public void generate(HttpServletResponse response) throws IOException, WriterException {
+    public void generate(HttpServletResponse response) throws Exception {
         ServletOutputStream outputStream = response.getOutputStream();
         QrCodeHelper.generate(outputStream);
     }
--- util/src/main/java/com/github/shiayanga/qrcode/QrCodeHelper.java
+++ util/src/main/java/com/github/shiayanga/qrcode/QrCodeHelper.java
@@ -3,14 +3,14 @@ package com.github.shiayanga.qrcode;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
@@ -49,10 +49,45 @@ public class QrCodeHelper {
             }
         }
 
-        Graphics2D graphics = (Graphics2D) image.getGraphics();
-        graphics.setColor(Color.BLACK);
-        graphics.drawString("邀请码：XVYTAD", matrix.getWidth() / 2 - 35, matrix.getHeight() - 20);
+
         return image;
+    }
+
+    /**
+     * 生成图片
+     *
+     * @param matrix
+     * @param config
+     * @return
+     */
+    public static BufferedImage toBufferedImageWithLogo(BitMatrix matrix, MatrixToImageConfig config) throws Exception {
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        int logH = (int) (height*0.2);
+        int logW = (int) (width*0.2);
+
+        BufferedImage image = toBufferedImage(matrix, config);
+        String url = "http://192.168.0.134:21001/love-chat-image/春风交友图标96.png";
+        BufferedImage poster = ImageIO.read(new URL(url));
+
+        int x = (width - logW) / 2;
+        int y = (height - logH) / 2;
+
+        BufferedImage resultImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphics = resultImage.createGraphics();
+
+        graphics.drawImage(image,0,0,null);
+        graphics.drawImage(poster,x,y,logW,logH,null);
+        Shape shape = new RoundRectangle2D.Float(x, y, logW, logH, 50, 30);
+        graphics.setStroke(new BasicStroke(2.5f));
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        graphics.draw(shape);
+        graphics.setColor(Color.BLACK);
+        graphics.setFont(new Font("黑体",Font.BOLD,24));
+        graphics.drawString("我的邀请码:XVYTAD", matrix.getWidth() / 2 - 90, matrix.getHeight() - 15);
+        graphics.dispose();
+
+        return resultImage;
     }
 
     /**
@@ -83,16 +118,38 @@ public class QrCodeHelper {
         }
     }
 
-    public static void generate(OutputStream outputStream) throws WriterException, IOException {
+    /**
+     * 写出图片到流
+     *
+     * @param matrix
+     * @param format
+     * @param outputStream
+     * @param config
+     * @throws IOException
+     */
+    public static void writeToStreamWithLogo(BitMatrix matrix, String format, OutputStream outputStream, MatrixToImageConfig config) throws Exception {
+        BufferedImage image = toBufferedImageWithLogo(matrix, config);
+        if (!ImageIO.write(image, format, outputStream)) {
+            throw new IOException("Could not write an image of format " + format);
+        }
+    }
+
+
+    public static void generate(OutputStream outputStream) throws Exception {
         String content = "http://www.baidu.com";
         int width = 500;
         int height = 500;
-        String format = "jpg";
+        String format = "png";
         HashMap<EncodeHintType, Object> hints = new HashMap<>();
+        // 指定字符集
         hints.put(EncodeHintType.CHARACTER_SET, StandardCharsets.UTF_8);
-        hints.put(EncodeHintType.MARGIN, 4);
+        // 指定边缘的宽度
+        hints.put(EncodeHintType.MARGIN, 3);
+        // 指定纠错级别。纠错级别越高，可以修正的错误就越多，但二维码的密度也越大。可选值包括L，M，Q和H，表示从7%到30%的纠错能力
+        hints.put(EncodeHintType.ERROR_CORRECTION, "H");
+
         BitMatrix matrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, width, height, hints);
 
-        writeToStream(matrix, format, outputStream);
+        writeToStreamWithLogo(matrix, format, outputStream,DEFAULT_CONFIG);
     }
 }
