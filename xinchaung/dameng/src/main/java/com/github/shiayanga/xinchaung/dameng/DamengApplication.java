2ef0d83235e35d84c46041d44f3bba4a35d53b44
和信金谷-李洋 <1499271883@qq.com>
2023-08-25 11:23:07 +0800

    达梦数据库

--- xinchaung/dameng/src/main/java/com/github/shiayanga/xinchaung/dameng/DamengApplication.java
+++ xinchaung/dameng/src/main/java/com/github/shiayanga/xinchaung/dameng/DamengApplication.java
@@ -1,9 +1,14 @@
 package com.github.shiayanga.xinchaung.dameng;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
+@Configuration
+@MapperScan(basePackages = "com.github.shiayanga.xinchaung.dameng.mapper")
 public class DamengApplication {
 
     public static void main(String[] args) {
--- xinchaung/dameng/src/main/java/com/github/shiayanga/xinchaung/dameng/controller/DmController.java
+++ xinchaung/dameng/src/main/java/com/github/shiayanga/xinchaung/dameng/controller/DmController.java
@@ -1,6 +1,9 @@
 package com.github.shiayanga.xinchaung.dameng.controller;
 
 
+import com.github.shiayanga.xinchaung.dameng.entity.City;
+import com.github.shiayanga.xinchaung.dameng.entity.CityExample;
+import com.github.shiayanga.xinchaung.dameng.mapper.CityMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +21,16 @@ import java.util.List;
 public class DmController {
     @Resource
     private JdbcTemplate jdbcTemplate;
+
+    @Resource
+    CityMapper cityMapper;
+
     @GetMapping("/queryDbVersion")
-    public Integer queryDbVersion(){
-        return jdbcTemplate.queryForObject("select count(1) from DMHR.DEPARTMENT", Integer.class);
+    public City queryDbVersion(){
+        CityExample cityExample = new CityExample();
+        CityExample.Criteria criteria = cityExample.createCriteria();
+        criteria.andCityIdEqualTo("BJ");
+        List<City> cities = cityMapper.selectByExample(cityExample);
+        return cities.get(0);
     }
 }
